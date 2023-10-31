ab677918c69c5ea93e8ba141e762b4b4c7d103a6
Theodor Johansson <t.j.u@telia.com>
2023-10-31 15:10:30 +0100

    Edited mongo_uri

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session')
 const passport = require('passport')
 const { ObjectID } = require('mongodb');
-
+// Edited mongo uri
 
 const app = express();
 
